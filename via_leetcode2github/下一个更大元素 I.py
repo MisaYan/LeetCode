79662e33847e6e4b48c1f7d4a7d6ada42fb551e3
MisaYan <52661398+MisaYan@users.noreply.github.com>
2020-03-30 10:38:26 +0800

    uploaded by emmmmmm with runtime 84 ms

---
+++ via_leetcode2github/下一个更大元素 I.py
@@ -0,0 +1,10 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        #stack & hash map
+        stack = []
+        hash = {}
+        for n in nums2:
+            while stack and stack[-1] < n:
+                hash[stack.pop()] = n
+            stack.append(n)
+        return [hash.get(x, -1) for x in nums1]
