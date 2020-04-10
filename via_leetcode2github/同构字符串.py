39156d408c39e2af4546d9bc59f72d715091920b
MisaYan <52661398+MisaYan@users.noreply.github.com>
2020-04-10 15:58:53 +0800

    uploaded by emmmmmm with runtime 76 ms

---
+++ via_leetcode2github/同构字符串.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        hashmap = {}
+        for i,j in zip(s,t):
+            if i in hashmap and hashmap[i] != j:
+                return False
+            elif i not in hashmap and j in hashmap.values():
+                return False
+            hashmap[i] = j
+        return True
+
+
