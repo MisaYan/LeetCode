171b656ed0c6cdf77ac9b5ba4288f30627cb5b6c
MisaYan <52661398+MisaYan@users.noreply.github.com>
2020-04-10 09:59:24 +0800

    uploaded by emmmmmm with runtime 124 ms

---
+++ via_leetcode2github/计数质数.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        #埃式筛法
+        if n <= 1:
+            return 0
+        isPrime = [1] * n
+        isPrime[0] = isPrime[1] = 0
+
+        for i in range(2, int(n**0.5) + 1):
+            if isPrime[i]:
+                isPrime[i*i:n:i] = [0] * ((n-1 - i*i) // i +1)
+        return sum(isPrime)
