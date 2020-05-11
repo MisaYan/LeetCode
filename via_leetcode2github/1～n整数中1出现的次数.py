81908d2eea28bc5b24058fc5056fcacf309cd272
MisaYan <52661398+MisaYan@users.noreply.github.com>
2020-05-11 17:59:10 +0800

    uploaded by emmmmmm with runtime N/A

---
+++ via_leetcode2github/1～n整数中1出现的次数.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        digit = 1
+        res = 0
+        high = n // 10
+        cur = n % 10
+        low = 0
+        while high != 0 or cur != 0:
+            if cur == 0:
+                res += high * digit
+            elif cur == 1:
+                res += high * digit + low + 1
+            else:
+                res += (high + 1) * digit
+            digit *= 10
+            low += cur * digit
+            high //= 10
+        return res
+
+
