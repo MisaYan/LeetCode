e39a0a4a0d5f2187d002ecb3f481b47635db0dd5
MisaYan <52661398+MisaYan@users.noreply.github.com>
2020-05-17 15:58:34 +0800

    uploaded by emmmmmm with runtime 48 ms

--- via_leetcode2github/二叉树的深度.py
+++ via_leetcode2github/二叉树的深度.py
@@ -6,10 +6,30 @@
 #         self.right = None
 
 class Solution:
+    #递归
     def maxDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+    #BFS
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        treeHeight = 0
+        queue = [root]      ###通过[]将某一层的树取出
+        while queue:
+            next_queue = []
+            treeHeight += 1
+            for node in queue:
+                if not node:
+                    continue
+                if node.left:
+                    next_queue.append(node.left)
+                if node.right:
+                    next_queue.append(node.right)
+            queue = next_queue
+        return treeHeight
 
 
 
